59be403c3902af1383b11a96c8d13b5fae66a81e
James Cornall <jjc257@student.bham.ac.uk>
2023-09-05 23:59:05 +0100

    Add basic PostgreSQL functionality

--- web_app.py
+++ web_app.py
@@ -1,12 +1,25 @@
 from flask import Flask, abort, render_template, redirect, url_for
 from markupsafe import escape
+from sqlalchemy import create_engine
+import psycopg2 as ps2
 
 app = Flask(__name__)
 
+_DATABASE_URL = "postgresql://postgres:password@127.0.0.1:5432/predicted_data"
+conn = ps2.connect(_DATABASE_URL)
+
 @app.route("/")
 @app.route("/index/")
-def hello():
-    return render_template("index.html")
+def index():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM speech_data")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("index.html", data = data)
+
+@app.teardown_appcontext
+def close_connection(exception):
+    conn.close()
 
 if __name__ == "__main__":
     app.run()
