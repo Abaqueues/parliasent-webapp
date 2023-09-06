6c1cfb927b17b42267f32f8e0355a2e5c2d87a01
James Cornall <jjc257@student.bham.ac.uk>
2023-09-06 01:50:24 +0100

    Implement form and data formatting

--- web_app.py
+++ web_app.py
@@ -1,25 +1,45 @@
-from flask import Flask, abort, render_template, redirect, url_for
+import os
+from flask import Flask, abort, render_template, redirect, url_for, request
 from markupsafe import escape
 from sqlalchemy import create_engine
+from sqlalchemy.sql import func
 import psycopg2 as ps2
 
 app = Flask(__name__)
 
-_DATABASE_URL = "postgresql://postgres:password@127.0.0.1:5432/predicted_data"
-conn = ps2.connect(_DATABASE_URL)
+_DATABASE_URI = "postgresql://postgres:password@127.0.0.1:5432/predicted_data"
 
 @app.route("/")
 @app.route("/index/")
 def index():
+    return render_template("index.html")
+
+@app.route("/data/")
+def data():
+    conn = ps2.connect(_DATABASE_URI)
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM speech_data")
+    cursor.execute("SELECT index, id, speakername, url, date, lr_prediction FROM speech_data")
     data = cursor.fetchall()
     cursor.close()
-    return render_template("index.html", data = data)
+    return render_template("data.html", data = data) 
 
-@app.teardown_appcontext
-def close_connection(exception):
-    conn.close()
+@app.route("/search/")
+def search():
+    return render_template("form.html")
+
+@app.route("/process_query/", methods=["GET", "POST"])
+def process_form():
+    user_input = request.form.get('user_input')
+    conn = ps2.connect(_DATABASE_URI)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT index, id, speakername, url, date, lr_prediction FROM speech_data WHERE speakername = '{user_input}'")
+    data = cursor.fetchall()
+    cursor.close()
+    return render_template("data.html", data = data) 
+
+# @app.teardown_appcontext
+# def close_connection(exception):
+#     conn.close()
 
 if __name__ == "__main__":
     app.run()
